43daac162996ec527e43baf99aa49073aee30240
hungnx <hungnx.buca.vn>
2021-03-04 15:06:53 +0700

    fix

--- src/XH.BaseProject.API/XH.BaseProject.Common/Exceptions/MethodNotAllowedException.cs
+++ src/XH.BaseProject.API/XH.BaseProject.Common/Exceptions/MethodNotAllowedException.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using XH.BaseProject.Common.Exceptions;
 
 namespace Buca.Common.Exceptions
 {
--- src/XH.BaseProject.API/XH.BaseProject.Common/Exceptions/NotFoundException.cs
+++ src/XH.BaseProject.API/XH.BaseProject.Common/Exceptions/NotFoundException.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using XH.BaseProject.Common.Exceptions;
 
-namespace Buca.Common.Exceptions
+namespace XH.BaseProject.Common.Exceptions
 {
     public class NotFoundException : ExceptionBase
     {
